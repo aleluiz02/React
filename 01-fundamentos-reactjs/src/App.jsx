e41679e173bd8cf3a978dece15153ff3ca73a48d
Alexandre <alexandreluizolicshevis@gmail.com>
2025-08-07 12:28:59 -0300

    Reescrita do Header e Sidebar após rename da Branch e perda de arquivos

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,21 +1,31 @@
 import { Header } from "./components/header.jsx";
 import { Post } from "./Post"
+import { Sidebar } from "./components/sidebar.jsx";
 import './global.css';
+import styles from './App.module.css';
+
 
 export function App() {
 
   return (
     <div> 
       <Header />
-      <Post 
-        author="Alexandre Olicshevis" 
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta enim ab modi obcaecati dolorum quis, illo suscipit ratione voluptatem quam assumenda totam eius dolore reprehenderit odio, vero quibusdam amet." 
-      />
       
-      <Post 
-        author="Jane Doe" 
-        content="This is another post content, showcasing how to use the Post component with different props."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Alexandre Olicshevis" 
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta enim ab modi obcaecati dolorum quis, illo suscipit ratione voluptatem quam assumenda totam eius dolore reprehenderit odio, vero quibusdam amet." 
+          />
+          
+          <Post 
+            author="Jane Doe" 
+            content="This is another post content, showcasing how to use the Post component with different props."
+          />
+        </main>
+      </div>
+
     </div>
     
   )
