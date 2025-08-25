57da69d3b1b1841a045251eb2974f3154c665b6e
Alexandre <alexandreluizolicshevis@gmail.com>
2025-08-25 16:33:48 -0300

    Botão publicar(comentários) e estilizações relacionadas concluídos

--- 01-fundamentos-reactjs/src/components/post.jsx
+++ 01-fundamentos-reactjs/src/components/post.jsx
@@ -27,6 +27,14 @@ export function Post({ title, author }) {
                 <p><a href="">Teste.link</a></p>
 
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea placeholder='Deixe um comentário' />
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
         </article>
     ) 
 }
