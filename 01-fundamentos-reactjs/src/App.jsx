ffe4b42ad60e316e37df87d8d30bf835e9d51d8e
Alexandre <alexandreluizolicshevis@gmail.com>
2025-08-28 12:57:58 -0300

    Iterações de posts - Não concluído

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -4,12 +4,40 @@ import { Header } from "./components/header.jsx";
 import { Post } from "./components/post.jsx"
 import { Sidebar } from "./components/sidebar.jsx";
 import { Comment } from "./components/comment.jsx";
-
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/FernaCaroline.png',
+      name: 'Fernanda Caroline',  
+      role: 'Manager e namorada do Alexandre'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-08-18 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/Aleluiz02.png',
+      name: 'Alexandre Olicshevis',  
+      role: 'Manager e namorado da Fernanda'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-08-17 20:00:00'),
+  }
+]
 
 export function App() {
-
   return (
     <div> 
       <Header />
@@ -17,20 +45,18 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Alexandre Olicshevis" 
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta enim ab modi obcaecati dolorum quis, illo suscipit ratione voluptatem quam assumenda totam eius dolore reprehenderit odio, vero quibusdam amet." 
-          />
-          
-          <Post 
-            author="Jane Doe" 
-            content="This is another post content, showcasing how to use the Post component with different props."
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                title={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
-
-    </div>
-    
+    </div>    
   )
 }
 
--- 01-fundamentos-reactjs/src/components/post.jsx
+++ 01-fundamentos-reactjs/src/components/post.jsx
@@ -2,30 +2,23 @@ import styles from './post.module.css';
 import { Comment } from './comment.jsx';    
 import { Avatar } from './avatar.jsx';
 
-export function Post({ title, author }) {
+export function Post({author, publishedAt}) {
     return (   
         <article className={styles.post}>
             <header>
-
                 <div className={styles.author}>
-                    <Avatar 
-                        src='https://github.com/FernaCaroline.png'
-                    />
+                    <Avatar src={author.avatarUrl}/>
                     <div className={styles.authorInfo}>
-                        <strong>{author}</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="18 de Agosto às 17:13h" dateTime="2025-08-18 17:13:30">Publicado há 1h</time>
+                <time title="18 de Agosto às 17:13h" dateTime="2025-08-18 17:13:30">{publishedAt.toString()}</time>
 
             </header>
 
             <div className={styles.content}>
-
-                <p>{title}</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta enim ab modi obcaecati dolorum quis, illo suscipit ratione voluptatem quam assumenda totam eius dolore reprehenderit odio, vero quibusdam amet.</p>
-                <p><a href="">Teste.link</a></p>
 
             </div>
 
